a3b7470b50dfc7cef82c2d121b877fa7f3dbcf11
Ubuntu <ubuntu@ip-172-31-39-222.eu-west-3.compute.internal>
2021-09-18 12:45:01 +0000

    init

---
+++ terraform/main.tf
@@ -0,0 +1,62 @@
+provider "aws" {
+  region = "eu-west-3"
+}
+
+terraform {
+  backend "s3" {
+    bucket = "hurtov-oleksii-project-terraform"
+    key = "server/terraform.tfstate"
+    region = "eu-west-3"
+  }
+}
+#=====================================================
+
+data "aws_ami" "latest_ubuntu_linux" {
+  owners      = ["099720109477"]
+  most_recent = true
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+}
+#=========================================================
+resource "aws_eip" "my_static_ip" {
+  instance = aws_instance.WebServer.id
+}
+
+resource "aws_instance" "WebServer" {
+  ami = data.aws_ami.latest_ubuntu_linux.id
+  instance_type   = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.web.id]
+  tags = {
+    Name = "WebServer"
+  }
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_security_group" "web" {
+  name = "Dynamic Security Group"
+
+  dynamic "ingress" {
+    for_each = ["80", "22"]
+    content {
+      from_port   = ingress.value
+      to_port     = ingress.value
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name  = "Dynamic SecurityGroup"
+    Owner = "Alex Hurtov"
+  }
+}
---
+++ terraform/outputs.tf
@@ -0,0 +1,11 @@
+### The Ansible inventory file
+resource "local_file" "AnsibleInventory" {
+ content = templatefile("inventory.tmpl",
+ {
+  WebServer-ip = aws_eip.my_static_ip.public_ip,
+  WebServer-id = aws_instance.WebServer.id,
+  WebServer-name = aws_instance.WebServer.tags.Name
+ }
+ )
+ filename = "../ansible/inventory"
+}
